a7e9b4bce3782799005f9a388dfd029a2348de55
caibrown <caibrown@ucdavis.edu>
2020-03-01 23:29:31 -0800

    work in progress

--- iv_multilayer_network.m
+++ iv_multilayer_network.m
@@ -2,13 +2,28 @@
 
 load mnist_all.mat
 
-input = test0;
-w1 = rand(size(input)) % weights mapping input layer to hidden layer
+test = test0;
+nNeurons = 20 % arbitrary
+nHidLayers = 1 % should be variable
+w1 = rand(size(test)) % weights mapping input layer to hidden layer..sizing (?)
 %weights = {w1,w2}
+target = 0 % corresponds to test(#) and will be used in error calculation
 
-function output = multiLayerNetwork(input, weights)
+function output = multiLayerNetwork(test, weights, nNeurons, nHidLayers)
 
-function NET = computeNET(input, w1)
-NET = sum(input.*w1)
+    for i=1:nNeurons
+    NET = computeNET(test(i,:),w1(i,:));
+    output = f(NET);
+    end
+    
+end  
+
+% Local fxns:
+function NET = computeNET(inputs, inputWeights)
+NET = sum(inputs.*inputWeights)
+end
+
+function OUT = f(NET)
+OUT = 1/(1 + exp(-NET)) % Sigmoidal activation function
 end
 
