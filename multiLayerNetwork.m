91795280c99cf203fd9bb69bd2c04e32d497feab
caibrown <caibrown@ucdavis.edu>
2020-03-04 19:19:55 -0800

    .

--- multiLayerNetwork.m
+++ multiLayerNetwork.m
@@ -1,14 +1,11 @@
 function output = multiLayerNetwork(input, weights, nHidLayers)
-% weights: nHidLayers + 1 cell array with each element a matrix of
-% dimension input by output
-
-% input must be column vector, converted to double from uint8
 x = input;
 
     for i=1:(nHidLayers + 1)
     x = computeNET(x, weights{i});
     x = f(x);
     end
+    
 output = x;   
 end
 
