3158ce304d2e53010797f11c417fa59bd7e197b1
caibrown <caibrown@ucdavis.edu>
2020-03-04 20:24:58 -0800

    pos and neg random values

--- initialize.m
+++ initialize.m
@@ -1,8 +1,8 @@
 function weights = initialize(nHidLayers, nNeurons)
 
 weights = cell(1, nHidLayers + 1);
-weights(1) = {rand(nNeurons,784)};
-weights(1,2:(end-1)) = {rand(nNeurons,nNeurons)};
-weights(end) = {rand(10,nNeurons)};
+weights(1) = {rand(nNeurons,784)-0.5};
+weights(1,2:(end-1)) = {rand(nNeurons,nNeurons)-0.5};
+weights(end) = {rand(10,nNeurons)-0.5};
 
 end
