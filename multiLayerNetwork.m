f3916e7467d0fec761b0bfd71b6197ff7840dc2b
caibrown <caibrown@ucdavis.edu>
2020-03-12 15:11:11 -0700

    works!

--- multiLayerNetwork.m
+++ multiLayerNetwork.m
@@ -2,7 +2,7 @@ function [x, output] = multiLayerNetwork(input, weights, nHidLayers)
 x = cell(1,nHidLayers+2);
 x(1) = {input};
 
-    for i=1:(nHidLayers)
+    for i=1:(nHidLayers+1)
     x(i+1) = {computeNET(x{i}, weights{i})};
     x(i+1) = {f(x{i+1})};
     end
@@ -12,7 +12,7 @@ end
 
 % Local fxns:
 function NET = computeNET(inputs, inputWeights)
-NET = weights*inputs;
+NET = inputWeights*inputs;
 end
 
 function OUT = f(NET)
--- train.m
+++ train.m
@@ -1,45 +1,66 @@
 %% VI. Training the Network
-%function output = train(input,target,nHidLayers,nNeurons,iterations)
-
 load mnist_all.mat
 
-input = double(train0(1,:))'; % must be column vect, converted to double <- uint8
-target = [1; 0; 0; 0; 0; 0; 0; 0; 0; 0];
-nHidLayers = 5; 
-nNeurons = 30;
-weights = initialize(nHidLayers, nNeurons);
-eta = 0.001; % training rate coeff, elem of [0.01, 0.1]
-% iterations = 100; 
-% 
-% for n = 1:iterations
-    
-x,output = multiLayerNetwork(input, weights, nHidLayers); % forward pass
-err = target - output;
-deltaOut = zeros(size(output)); % preallocate
+samples = 1500; % how many samples from each digit data-frame
+selectSamples = randi(5400,samples,1);
+inputCell = {...
+    double(train0(selectSamples,:))',...
+    double(train1(selectSamples,:))',...
+    double(train2(selectSamples,:))',...
+    double(train3(selectSamples,:))',...
+    double(train4(selectSamples,:))',...
+    double(train5(selectSamples,:))',...
+    double(train6(selectSamples,:))',...
+    double(train7(selectSamples,:))',...
+    double(train8(selectSamples,:))',...
+    double(train9(selectSamples,:))',...
+    };
 
-for i=1:length(output) % d/dx sigmoidal * error
-deltaOut(i) = f(output(i)).*(1-f(output(i))).*(err(i));
+targetMatrix = eye(10);
+guess = zeros(10);
+nHidLayers = 5; 
+nNeurons = 50;
+weights = initialize(nHidLayers, nNeurons);
+eta = 0.01;
+
+for k=1:10
+    inputMatrix = inputCell{k}; % select matrix
+    target = targetMatrix(:,k);
+
+    for j=1:samples
+        input = inputMatrix(:,j); % cycle thru cols
+    
+        [x,output] = multiLayerNetwork(input, weights, nHidLayers);
+        err = target - output;
+        deltaOut = zeros(size(output)); % preallocate
+
+        for i=1:length(output) % d/dx sigmoid * error
+            deltaOut(i) = f(output(i)).*(1-f(output(i))).*(err(i));
+        end
+
+        delta = cell(size(weights));
+        delta(end) = {deltaOut};
+
+        for i = length(weights):-1:2
+   
+            delta{i-1} = zeros(size(x{i-1})); % preallocate
+            delta{i-1} = sum(weights{i}'*delta{i})*...
+                (f(x{i-1}).*(1-f(x{i-1})));
+
+            weights{i} = weights{i} + eta*delta{i}*x{i}';
+        end    
+    end
+    
+    guess(:,k) = output;
 end
 
-delta = cell(size(weights));
-delta = [delta {deltaOut}];
-
-for i = length(weights):-1:1
-    delta{i} = zeros(size(x{i})); % preallocate
-    
-    delta{i} = sum(weights{i}'*delta{i+1})*...
-        (f(x{i}).*(1-f(x{i})));
-
-    weights{i} = eta * delta{i} .* x{i};
-end    
-% end
-
-disp('Network''s guess:');disp(output)
-
-
+disp('~Results~')
+for l=1:10
+    disp('Target:');disp(l-1)
+    disp('Network''s guess:');disp(guess(:,l))
+end
 
 % local fxns
 function OUT = f(NET)
-OUT = 1./(1 + exp(-NET)); % Sigmoidal activation function
-%OUT = OUT';
+OUT = 1./(1 + exp(-NET));
 end
